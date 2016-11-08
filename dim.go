098b6c3bef8501c3fde206293b32454a844bec67
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-11-08 10:52:30 +0100

    Follow file structure of go/src/math

--- dim.go
+++ dim.go
@@ -2,16 +2,6 @@ package math32
 
 import "math"
 
-var (
-	Epsilon32 float32
-	NaN       float32
-)
-
-func init() {
-	Epsilon32 = math.Nextafter32(1, 2) - 1
-	NaN = float32(math.NaN())
-}
-
 // Abs returns the absolute value of x.
 //
 // Special cases are:
@@ -50,10 +40,4 @@ func Min(x, y float32) float32 {
 //	Dim(x, NaN) = Dim(NaN, x) = NaN
 func Dim(x, y float32) float32 {
 	return float32(math.Dim(float64(x), float64(y)))
-}
-
-// Approx returns true if x ~= y
-func Approx(x, y float32) bool {
-	eps := Epsilon32 * 100
-	return Abs(x-y) < eps*(1.0+Max(Abs(x), Abs(y)))
 }
