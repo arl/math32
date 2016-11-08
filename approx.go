6fd99a8b6bd9ef7f218129c62636395dfea9a7be
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-11-09 00:17:11 +0100

    Golint

--- approx.go
+++ approx.go
@@ -3,15 +3,15 @@ package math32
 import "math"
 
 var (
-	Epsilon32 float32
+	epsilon32 float32
 )
 
 func init() {
-	Epsilon32 = math.Nextafter32(1, 2) - 1
+	epsilon32 = math.Nextafter32(1, 2) - 1
 }
 
 // Approx returns true if x ~= y
 func Approx(x, y float32) bool {
-	eps := Epsilon32 * 100
+	eps := epsilon32 * 100
 	return Abs(x-y) < eps*(1.0+Max(Abs(x), Abs(y)))
 }
