00b698a4e4e94a453e0ed4b56fca7a248285b855
Aurélien Rainone <aurelien.rainone@gmail.com>
2020-04-05 12:37:17 +0200

    Update Pow10() doc

--- pow10.go
+++ pow10.go
@@ -2,11 +2,11 @@ package math32
 
 import "math"
 
-// Pow10 returns 10**e, the base-10 exponential of e.
+// Pow10 returns 10**n, the base-10 exponential of n.
 //
 // Special cases are:
-//	Pow10(e) = +Inf for e > 309
-//	Pow10(e) = 0 for e < -324
+//	Pow10(n) =    0 for n < -323
+//	Pow10(n) = +Inf for n > 308
 func Pow10(e int) float32 {
 	return float32(math.Pow10(e))
 }
